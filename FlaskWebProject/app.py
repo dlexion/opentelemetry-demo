5610cfa2debab29de1ea7e50da0d526bd2116bc9
Aliaksei Dubinevich <Aliaksei_Dubinevich@epam.com>
2021-04-28 14:14:39 +0300

    metric exporting working example

--- FlaskWebProject/app.py
+++ FlaskWebProject/app.py
@@ -12,7 +12,7 @@ wsgi_app = app.wsgi_app
 import requests
 
 from opentelemetry import trace
-from opentelemetry.exporter.opencensus.trace_exporter import OpenCensusSpanExporter
+# from opentelemetry.exporter.opencensus.trace_exporter import OpenCensusSpanExporter
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import (
@@ -23,24 +23,40 @@ from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 
-# from opentelemetry import metrics
-# from opentelemetry.exporter.opencensus.metrics_exporter import (
-#     OpenCensusMetricsExporter,
-# )
-# from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry import metrics
+# from opentelemetry.exporter.otlp.proto.grpc.metrics_exporter import OTLPMetricsExporter
+from opentelemetry.exporter.opencensus.metrics_exporter import OpenCensusMetricsExporter
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export.controller import PushController #need it?
 
 import logging
 from opentelemetry.instrumentation.logging import LoggingInstrumentor
-
+#tracer setup
 tracer_provider = TracerProvider(resource=Resource.create({SERVICE_NAME: "python_service_traces"}))
 trace.set_tracer_provider(tracer_provider)
 exporter = OTLPSpanExporter(endpoint="http://localhost:4317")
 span_processor = BatchSpanProcessor(exporter)
 tracer_provider.add_span_processor(span_processor)
 
-tracer = trace.get_tracer(__name__)
-logger = logging.getLogger(__name__)
+#metrics setup
+exporter = OpenCensusMetricsExporter(
+    service_name="basic-service", endpoint="localhost:55679"
+)
+metrics.set_meter_provider(MeterProvider())
+meter = metrics.get_meter(__name__)
+# metrics.get_meter_provider().start_pipeline(meter, exporter, 5)
+controller = PushController(meter, exporter, 5)
 
+requests_counter = meter.create_counter(
+    name="requests",
+    description="number of requests",
+    unit="1",
+    value_type=int,
+)
+
+staging_labels = {"environment": "staging"}
+
+#instrumentation
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
 LoggingInstrumentor().instrument(set_logging_format=True, logging_format=
@@ -53,6 +69,9 @@ span_id:\t%(otelSpanID)s
 service.name:\t%(otelServiceName)s
 message:\t%(message)s\n""")
 
+tracer = trace.get_tracer(__name__)
+logger = logging.getLogger(__name__)
+
 @app.route('/')
 def hello():
     flask_span = trace.get_current_span()
@@ -63,6 +82,7 @@ def hello():
         span.add_event('Python event')
         logger.warning("warning inside custom span")
         requests.get("http://www.example.com")
+    requests_counter.add(25, staging_labels)
     return "Hello from Python app!"
 
 if __name__ == '__main__':
