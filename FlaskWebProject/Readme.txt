6670902bf2e07511da170dbef6dfc6cce4c2be63
dlexion <dlexion@yandex.ru>
2023-08-15 11:51:39 +0200

    Last cahnges & get it working now

--- FlaskWebProject/Readme.txt
+++ FlaskWebProject/Readme.txt
@@ -12,3 +12,9 @@ python app.py
 
 to save requirements (in case of update):
 pip freeze > requirements.txt
+
+does not work:
+docker build -t python-otl-sample .
+docker run -d -p 5555:8080 python-otl-sample
+
+kubectl apply -f k8s.yaml
--- FlaskWebProject/app.py
+++ FlaskWebProject/app.py
@@ -10,77 +10,80 @@ app = Flask(__name__)
 wsgi_app = app.wsgi_app
 
 import requests
+import os
 
 from opentelemetry import trace
-from opentelemetry.exporter.opencensus.trace_exporter import OpenCensusSpanExporter
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import (
     BatchSpanProcessor,
-    SimpleSpanProcessor,
+    ConsoleSpanExporter
     )
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 
-from opentelemetry import metrics
-from opentelemetry.exporter.otlp.proto.grpc.metrics_exporter import OTLPMetricsExporter
-from opentelemetry.sdk.metrics import MeterProvider
-from opentelemetry.sdk.metrics.export.controller import PushController
+# from opentelemetry import metrics
+# from opentelemetry.exporter.otlp.proto.grpc.metrics_exporter import OTLPMetricsExporter
+# from opentelemetry.sdk.metrics import MeterProvider
+# from opentelemetry.sdk.metrics.export.controller import PushController
 
 import logging
 from opentelemetry.instrumentation.logging import LoggingInstrumentor
 
 #tracer setup
-tracer_provider = TracerProvider(resource=Resource.create({SERVICE_NAME: "python_service_traces"}))
+tracer_provider = TracerProvider(resource=Resource.create({SERVICE_NAME: "uv-opentelemetry-python-sample"}))
 trace.set_tracer_provider(tracer_provider)
-exporter = OpenCensusSpanExporter(endpoint="localhost:55679")
+TRACE_EXPORTER_ENDPOINT = os.environ.get('SERVER_TRACE_EXPORTER_ENDPOINT', 'http://localhost:4317')
+exporter = OTLPSpanExporter(endpoint=TRACE_EXPORTER_ENDPOINT, insecure=True)
 span_processor = BatchSpanProcessor(exporter)
+# span_processor = BatchSpanProcessor(ConsoleSpanExporter())
 tracer_provider.add_span_processor(span_processor)
 
-#metrics setup
-metrics_exporter = OTLPMetricsExporter(endpoint="localhost:4317", insecure=True)
-metrics.set_meter_provider(MeterProvider())
-meter = metrics.get_meter(__name__)
-controller = PushController(meter, metrics_exporter, 5)
+# #metrics setup
+# metrics_exporter = OTLPMetricsExporter(endpoint="localhost:4317", insecure=True)
+# metrics.set_meter_provider(MeterProvider())
+# meter = metrics.get_meter(__name__)
+# controller = PushController(meter, metrics_exporter, 5)
 
-requests_counter = meter.create_counter(
-    name="requests",
-    description="number of requests",
-    unit="1",
-    value_type=int,
-)
+# requests_counter = meter.create_counter(
+#     name="requests",
+#     description="number of requests",
+#     unit="1",
+#     value_type=int,
+# )
 
-staging_labels = {"environment": "staging"}
+# staging_labels = {"environment": "staging"}
 
-import psutil
-# Callback to gather cpu usage
-def get_cpu_usage_callback(observer):
-    for (number, percent) in enumerate(psutil.cpu_percent(percpu=True)):
-        labels = {"cpu_number": str(number)}
-        observer.observe(percent, labels)
-
-
-meter.register_valueobserver(
-    callback=get_cpu_usage_callback,
-    name="cpu_percent",
-    description="per-cpu usage",
-    unit="1",
-    value_type=float,
-)
-
-# Callback to gather RAM memory usage
-def get_ram_usage_callback(observer):
-    ram_percent = psutil.virtual_memory().percent
-    observer.observe(ram_percent, {})
+# import psutil
+# # Callback to gather cpu usage
+# def get_cpu_usage_callback(observer):
+#     for (number, percent) in enumerate(psutil.cpu_percent(percpu=True)):
+#         labels = {"cpu_number": str(number)}
+#         observer.observe(percent, labels)
 
 
-meter.register_valueobserver(
-    callback=get_ram_usage_callback,
-    name="ram_percent",
-    description="RAM memory usage",
-    unit="1",
-    value_type=float,
-)
+# meter.register_valueobserver(
+#     callback=get_cpu_usage_callback,
+#     name="cpu_percent",
+#     description="per-cpu usage",
+#     unit="1",
+#     value_type=float,
+# )
+
+# # Callback to gather RAM memory usage
+# def get_ram_usage_callback(observer):
+#     ram_percent = psutil.virtual_memory().percent
+#     observer.observe(ram_percent, {})
+
+
+# meter.register_valueobserver(
+#     callback=get_ram_usage_callback,
+#     name="ram_percent",
+#     description="RAM memory usage",
+#     unit="1",
+#     value_type=float,
+# )
 
 #instrumentation
 FlaskInstrumentor().instrument_app(app)
@@ -102,18 +105,16 @@ logger = logging.getLogger(__name__)
 def hello():
     flask_span = trace.get_current_span()
     flask_span.set_attribute("custom_info", "add more attributes to the flask instrumented span")
-    logger.info("info inside flask instrumented span")
+    logger.info("log info inside Flask instrumented span")
     with tracer.start_as_current_span("example-request") as span:
         span.set_attribute("is_example", "yes :)")
         span.add_event('Python event')
-        logger.warning("warning inside custom span")
+        logger.warning("log warning inside custom span")
         requests.get("http://www.example.com")
-    requests_counter.add(25, staging_labels)
     return "Hello from Python app!"
 
 if __name__ == '__main__':
-    import os
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    HOST = os.environ.get('SERVER_HOST', '0.0.0.0')
     try:
         PORT = int(os.environ.get('SERVER_PORT', '5555'))
     except ValueError:
